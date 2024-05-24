63629a4f9e7abef12e2ff658c1e7afd04dfae99c
brian <briansuyker@gmail.com>
2024-05-24 13:58:50 +0200

    ik heb voor een opsla gedeelte gezorgt voor de chats alleen kan ik die nog niet helemaal af maken tot de gui af is

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
-
 import javax.swing.*;
-import java.awt.*;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Main extends JFrame {
 
@@ -19,6 +18,22 @@ public class Main extends JFrame {
                 new Main();
             }
         });
+        try{
+            ChatMessage msg1 = new ChatMessage("Alice", "Hello!");
+            ChatMessage msg2 = new ChatMessage("Bob", "Hi Alice!");
+        ChatBox chatBox = new ChatBox(Arrays.asList(msg1, msg2));
 
+        chatBox.writeToCsvFile("chatbox.csv");
+
+        ChatBox loadedChatBox = ChatBox.readFromCsvFile("chatbox.csv");
+
+        for (ChatMessage message : loadedChatBox.getMessages()) {
+            System.out.println(message.getSender() + ": " + message.getMessage());
+        }
+    } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
+
