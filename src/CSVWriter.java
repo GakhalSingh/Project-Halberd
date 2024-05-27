41327f55f7c5df4212b5c6bc5ec0d9fcae3a18d4
brian <briansuyker@gmail.com>
2024-05-27 12:30:07 +0200

    inlog heeft nu ook een email toegevoegt

--- src/CSVWriter.java
+++ src/CSVWriter.java
@@ -1,7 +1,6 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 
 public class CSVWriter {
     private String bestandsnaam;
@@ -10,9 +9,9 @@ public class CSVWriter {
         this.bestandsnaam = bestandsnaam;
     }
 
-    public void CSVAccountadder(String gebruikersnaam, String wachtwoord) {
+    public void CSVAccountadder(String gebruikersnaam, String email, String wachtwoord) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.bestandsnaam, true))) {
-            writer.write(gebruikersnaam + "," + wachtwoord + System.lineSeparator());
+            writer.write(gebruikersnaam + "," + email+ "," + wachtwoord + System.lineSeparator());
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/Login.java
+++ src/Login.java
@@ -6,18 +6,20 @@ public class Login extends Account {
     private CSVWriter csvWriter;
     private CSVReader csvReader;
 
-    public Login(String userName, String wachtWoord, String csvFilePath) {
-        super(userName, wachtWoord);
+    public Login(String userName, String email, String wachtWoord, String csvFilePath) {
+        super(userName, email, wachtWoord);
         this.scanner = new Scanner(System.in);
         this.csvWriter = new CSVWriter(csvFilePath);
         this.csvReader = new CSVReader(csvFilePath);
     }
 
     public void loginScreen() {
-        String gebruikersnaam = scanner.nextLine();
+        System.out.print("Gebruikersnaam of e-mail: ");
+        String gebruikersnaamOfEmail = scanner.nextLine();
+        System.out.print("Wachtwoord: ");
         String wachtwoord = scanner.nextLine();
 
-        Account account = new Account(gebruikersnaam, wachtwoord);
+        Account account = new Account(gebruikersnaamOfEmail, gebruikersnaamOfEmail.contains("@") ? gebruikersnaamOfEmail : null, wachtwoord);
 
         if (isValidUser(account)) {
             System.out.println("Login succesvol!");
@@ -28,19 +30,27 @@ public class Login extends Account {
 
     private boolean isValidUser(Account account) {
         Map<String, String> accounts = csvReader.readAccounts();
-        return accounts.containsKey(account.getUserName()) && accounts.get(account.getUserName()).equals(account.getWachtwoord());
+
+        // Check if the input is an email or username and validate accordingly
+        if (account.getEmail() != null) {
+            return accounts.containsKey(account.getEmail()) && accounts.get(account.getEmail()).equals(account.getWachtwoord());
+        } else {
+            return accounts.containsKey(account.getUserName()) && accounts.get(account.getUserName()).equals(account.getWachtwoord());
+        }
     }
 
     public void nieuwAccount() {
         System.out.println("Maak een gebruikersnaam");
         String nieuweNaam = scanner.nextLine();
+        System.out.println("wat is je email");
+        String email = scanner.nextLine();
         System.out.println("Je nieuwe wachtwoord");
         String nieuwWachtwoord = scanner.nextLine();
         System.out.println("Herhaal het wachtwoord");
         String nieuwWachtwoord2 = scanner.nextLine();
 
         if (nieuwWachtwoord.equals(nieuwWachtwoord2)) {
-            csvWriter.CSVAccountadder(nieuweNaam, nieuwWachtwoord);
+            csvWriter.CSVAccountadder(nieuweNaam, email, nieuwWachtwoord);
             System.out.println("Account aangemaakt voor: " + nieuweNaam);
         } else {
             System.out.println("Wachtwoorden komen niet overeen, probeer opnieuw.");
